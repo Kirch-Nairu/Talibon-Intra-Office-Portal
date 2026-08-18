573c282452049556ea588142b15d326e401a162c
Kirch Ivan Balite <kirchbalite.careers@gmail.com>
2026-08-19 01:32:56 +0800

    seed: include payroll prototype data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             LegislativeDemoSeeder::class,
             HrisDemoSeeder::class,
             OperationsMonitoringDemoSeeder::class,
+            PayrollDemoSeeder::class,
         ]);
     }
 }
